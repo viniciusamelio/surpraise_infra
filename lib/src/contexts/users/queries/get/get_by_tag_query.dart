36c88dcbb86cf4f144253c145da89696037e4021
Vinicius Amélio <vinicius.amelio@zoho.com>
2023-10-11 14:43:29 -0300

    chore⚙️: exporting user dtos

--- lib/src/contexts/users/queries/get/get_by_tag_query.dart
+++ lib/src/contexts/users/queries/get/get_by_tag_query.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:ez_either/ez_either.dart';
 import 'package:surpraise_infra/src/contexts/collections.dart';
-import 'package:surpraise_infra/src/contexts/users/dtos/dtos.dart';
 
 import '../../../../../surpraise_infra.dart';
 
--- lib/src/contexts/users/users.dart
+++ lib/src/contexts/users/users.dart
@@ -1,3 +1,4 @@
 export "./mappers/user_mapper.dart";
 export "./queries/queries.dart";
 export "./repositories/user_repository.dart";
+export "dtos/dtos.dart";
