bb797335b82b9be769c10d9793d3b6f10c87e124
Vinicius Amélio <vinicius.amelio@zoho.com>
2023-10-11 14:18:22 -0300

    chore⚙️: exporting role enum

--- lib/src/surpraise_infra_base.dart
+++ lib/src/surpraise_infra_base.dart
@@ -4,5 +4,6 @@ export "./external/external.dart";
 export "./failures/failures.dart";
 export "./query/query.dart";
 export "./id/uuid_service.dart";
+export "enums/enums.dart";
 
 export 'package:surpraise_core/surpraise_core.dart' show StreamEventBus;
